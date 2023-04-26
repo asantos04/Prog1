891a1fb73119eabd0f555bd1c719babc32235f0f
asantos04 <andre.santos155@gmail.com>
2023-04-26 15:41:55 +0100

    exercícios

---
+++ P00/divisors.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int d=1 ;d <= n-1; d++){
+        if (n % d == 0) {
+            cout << d << ' ';
+        }
+
+    }
+    return 0;
+}
