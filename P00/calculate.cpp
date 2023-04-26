73040a58192b28936b95cacf45837669fe9090cb
asantos04 <andre.santos155@gmail.com>
2023-04-26 16:10:00 +0100

    P00 calculate

--- P00/calculate.cpp
+++ P00/calculate.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+  int a = 0;
   int x; // Declares x as a variable of type int.
   cin >> x; // Reads input value for x.
   int y = x*x -x + 1; // TODO: edit the assignment expression.
